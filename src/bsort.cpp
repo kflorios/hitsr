d28c0e3b272b6e28582e8c0d6610c69151df943f
Kostas Florios <cflorios@yahoo.com>
2026-09-08 19:13:27 +0300

    Add bubble sort function in bsort.cpp
    Implemented bubble sort algorithm to sort an array.

--- src/bsort.cpp
+++ src/bsort.cpp
@@ -1,1 +1,27 @@
+#include "tabu_search.hpp"
+
+// -----------------------------------------------------------------------------
+// bsort: sort nat[0..np-1] into snat[0..np-1] in increasing order
+// Fortran original:
+//   do i=1,np
+//   do i=1,np-1
+//      do j=i+1,np
+// -----------------------------------------------------------------------------
+void bsort(int np, const int nat[], int snat[])
+{
+    // copy input
+    for (int i = 0; i < np; ++i)
+        snat[i] = nat[i];
+
+    // simple bubble/selection hybrid as in Fortran
+    for (int i = 0; i < np - 1; ++i) {
+        for (int j = i + 1; j < np; ++j) {
+            if (snat[i] > snat[j]) {
+                int tmp = snat[i];
+                snat[i] = snat[j];
+                snat[j] = tmp;
+            }
+        }
+    }
+}
 
