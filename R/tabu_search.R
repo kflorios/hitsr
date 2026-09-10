a47dc1fe55874a2ae8efaa8c85156f87b13ef3ac
Kostas Florios <cflorios@yahoo.com>
2026-09-11 00:46:52 +0300

    Add files via upload

--- R/tabu_search.R
+++ R/tabu_search.R
@@ -3,7 +3,7 @@
 #' @param X Numeric matrix of dimensions (T, p_in).
 #' @param y Integer vector of length T containing binary target indicators.
 #' @param b0 Double, intercept parameter.
-#' @param d Integer parameter.
+#' @param d Double parameter.
 #' @param iSeed Integer, random number generator seed.
 #' @return A list containing best attributes, coefficients, and best score.
 #' @useDynLib hitsr, .registration = TRUE
@@ -15,6 +15,6 @@ run_tabu_search <- function(X, y, b0 = -1.0, d = 10, iSeed = 123456) {
   
   y_vec <- as.integer(y)
   
-  res <- cpp_run_tabu_search(X_mat, y_vec, b0, as.integer(d), as.integer(iSeed))
+  res <- cpp_run_tabu_search(X_mat, y_vec, b0, d, as.integer(iSeed))
   return(res)
 }
